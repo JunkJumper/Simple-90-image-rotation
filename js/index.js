eae705f0e6005f1ac6f90cfd02f85c321550f47d
JunkJumper <38364274+JunkJumper@users.noreply.github.com>
2018-05-25 08:56:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(".crossRotate").click(function () {
     //alert($( this ).css( "transform" ));
     if ($(this).css("transform") == 'none') {
-        $(this).css("transform", "rotate(900deg)");
+        $(this).css("transform", "rotate(3600deg)");
     } else {
         $(this).css("transform", "");
     }
